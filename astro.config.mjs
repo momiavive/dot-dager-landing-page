fa692f655d969f2380c4fe169a7f7400e6d8560a
Carlos Lázaro <ffclmauricio@outlook.es>
2024-11-23 01:46:04 -0500

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import paraglide from '@inlang/paraglide-astro';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
+  // output: "server",
+  integrations: [paraglide({
+    project: "./project.inlang",
+    outdir: "./src/paraglide"
+  }), tailwind()]
+});
---
+++ src/middleware/index.ts
@@ -0,0 +1,20 @@
+import { defineMiddleware } from 'astro:middleware';
+import {
+  languageTag,
+  setLanguageTag,
+  type AvailableLanguageTag,
+} from '../paraglide/runtime';
+
+export const onRequest = defineMiddleware((context, next) => {
+  // Get lang from url param
+  const lang = context.params.lang;
+
+  // If changed
+  if (lang !== languageTag()) {
+    setLanguageTag(lang as AvailableLanguageTag);
+    // Redirect to lang changed or default (en)
+    return context.redirect(`/${lang ?? 'en'}`);
+  }
+
+  return next();
+});

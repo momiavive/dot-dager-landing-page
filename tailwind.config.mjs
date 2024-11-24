15f36318f68fa6d69e95ffade3c6a4546894c7f1
Carlos Lázaro <ffclmauricio@outlook.es>
2024-11-23 22:37:15 -0500

    added some animation to axe icon and some enhancements

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+        wiggle: 'wiggle 2s ease-in-out infinite',
+      },
+			keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-30deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        }
+      }
+		},
 	},
 	plugins: [],
 }
